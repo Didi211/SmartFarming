d35b21c511d1c2dd76169b0d34ddc6021cd920d4
Didi211 <miticd99@gmail.com>
2023-11-08 00:26:34 +0100

    add services

---
+++ Backend/Cloud/Gateway/services/edge-gateway-service.js
@@ -0,0 +1,20 @@
+const addDevice = async (device) => { }
+
+const updateDevice = async (device) => { }
+
+const removeDevice = async (id) => { }
+
+const addRule = async (rule) => { }
+
+const updateRule = async (rule) => { }
+
+const removeRule = async (id) => { }
+
+export default { 
+    addDevice,
+    updateDevice,
+    removeDevice,
+    addRule,
+    updateRule,
+    removeRule
+}
