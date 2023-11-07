aaa1bbe3b1ad3ba70b00cd206f00ef0d9ee9da1b
Didi211 <miticd99@gmail.com>
2023-11-07 18:02:09 +0100

    add validators

---
+++ Backend/Cloud/Gateway/utils/device-validator.js
@@ -0,0 +1,46 @@
+
+
+const validate = (device) => { 
+    let result = "";
+    try { 
+        if (device.name == undefined || device.name == "") { 
+            throw "Device name is required."
+        }
+        if (device.type == undefined || device.type == "") { 
+            throw "Device type is required."
+        }
+        let typeUpperCase = device.type.toUpperCase();
+        if (typeUpperCase != 'SENSOR' && typeUpperCase != 'ACTUATOR') { 
+            throw "Device type must be either SENSOR or ACTUATOR."
+        }
+    }
+    catch(error) { 
+        result = error;
+    }
+    return result;
+}
+
+const validateForUpdate = (id, device) => { 
+    let result = "";
+    try { 
+        if (device.id == undefined || device.id == "") { 
+            throw "Device ID cannot be removed when updating device."
+        }
+        if (id != device.id) { 
+            throw "Device ID cannot be updated."
+        }
+        result = validate(device)
+        if (result != "") { 
+            throw result
+        }
+    }
+    catch(error) { 
+        result = error;
+    }
+    return result;
+}
+
+export default { 
+    validate,
+    validateForUpdate
+}
