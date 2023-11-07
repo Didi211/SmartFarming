f32b90e58f43acdbe002b1f674ab8d013e3bf4d5
Didi211 <miticd99@gmail.com>
2023-11-07 18:04:06 +0100

    update index js

--- Backend/Cloud/Gateway/index.js
+++ Backend/Cloud/Gateway/index.js
@@ -2,13 +2,19 @@ import cors from 'cors';
 import express, { json, urlencoded } from 'express';
 import dotenv from 'dotenv';
 // import { edgeGateway, persistence } from './axios-config';
+import deviceRoutes from './routes/device-routes.js';
+
 
 const app = express();
-    
+
 dotenv.config();
 
 app.use(json());
 app.use(urlencoded({extended: false}));
+app.use(cors());
+
+app.use('/api/devices',deviceRoutes);
+
 
 const port = process.env.PORT;
 app.listen(port, () => { 
