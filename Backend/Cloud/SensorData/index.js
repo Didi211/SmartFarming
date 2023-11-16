6a6c21ca5870f404e708be1f7e071ff413e5e14a
Didi211 <miticd99@gmail.com>
2023-11-16 21:26:31 +0100

    sensor data service init

---
+++ Backend/Cloud/SensorData/index.js
@@ -0,0 +1,24 @@
+import cors from 'cors';
+import express, { json, urlencoded } from 'express';
+import dotenv from 'dotenv';
+// import routes from './routes/routes.js'
+
+dotenv.config({path: 'sensor-data.env'});
+
+const app = express();
+
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(cors());
+
+// app.use('/api/sensor-data', routes);
+
+try { 
+    const port = process.env.PORT;
+    app.listen(port, () => { 
+        console.log(`Sensor Data Service is running on port ${port}`);
+    });
+}
+catch(error) { 
+    console.log(error);
+}
